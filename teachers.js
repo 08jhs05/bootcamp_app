123647f67ba1f208315bdc054dfc99d4ad041ed3
08jhs05 <08jhs05@gmail.com>
2021-04-28 23:27:08 +0000

    psql + js practice

--- teachers.js
+++ teachers.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 
 const cohort = process.argv[2];
-const maxResult = Number( process.argv[3] );
 
 const pool = new Pool({
   user: 'vagrant',
@@ -10,16 +9,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+console.log('connected');
+
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohort + '%'}'
-LIMIT ${maxResult};
+WHERE cohorts.name LIKE '%${cohort}%'
+ORDER BY teacher;
 `)
 .then(res => {
   for(const row of res.rows) {
-    console.log(`${row.name} has an id of ${row.student_id} and was in the ${row.cohort} cohort`);
+    console.log(`${cohort}: ${row.teacher}`);
   }
 })
 .catch(err => console.error('query error', err.stack));
